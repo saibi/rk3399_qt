ded4703a2d9843dc997954dd7e1f2258d266a645
Youngmin Kim <ymkim@huvitz.com>
2021-01-25 16:32:27 +0900

    qpa eglfs top window test

--- notify_test/main.cpp
+++ notify_test/main.cpp
@@ -2,12 +2,24 @@
 #include <QApplication>
 
 #include "notification.h"
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 
-	//Notification::instance().notify("Before MainWindow"); // uncomment this to reproduce full screen problem
+	qDebug("platformName : %s", qPrintable(QApplication::platformName()));
+
+	QWidget topWindow;
+
+	if (QApplication::platformName() == "eglfs")
+	{
+		topWindow.setStyleSheet("QWidget { background-color: black;}");
+		topWindow.showFullScreen();
+		topWindow.hide();
+	}
+
+	Notification::instance().notify("Before MainWindow"); // uncomment this to reproduce full screen problem
 	MainWindow w;
 	w.show();
 
