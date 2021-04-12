be10dba7081cef9da9b7b06a3af3129f1dfa19a5
Youngmin Kim <ymkim@huvitz.com>
2021-04-12 14:20:07 +0900

    qt5 rk3399 crosscompile helper scripts

---
+++ qt5_crosscompile_helper_script/5.15.2/old/c_20210118_32.sh
@@ -0,0 +1,48 @@
+#!/bin/bash 
+../configure -v -opensource -confirm-license \
+	-release \
+	-platform linux-g++-32 \
+	-device linux-rk3399-g++ \
+	-device-option CROSS_COMPILE=/opt/gcc-linaro-7.3.1-2018.05-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu- \
+	-nomake tests -nomake examples \
+	-no-cups -no-pch \
+	-iconv -no-icu \
+	-ssl \
+	-opengl es2 \
+	-qt-libpng -qt-libjpeg \
+	-sysroot /opt/rootfs-rk3399-ubuntu \
+	-prefix /opt/qt-5.15.2-rk3399-test32 \
+	-hostprefix /opt/qt-5.15.2-rk3399-test32 \
+	\
+	-skip qtactiveqt \
+	-skip qtandroidextras \
+	-skip qtcharts \
+	-skip qtconnectivity \
+	-skip qtdatavis3d \
+	-skip qtdoc \
+	-skip qtgamepad \
+	-skip qtimageformats \
+	-skip qtlocation \
+	-skip qtlottie \
+	-skip qtmacextras \
+	-skip qtnetworkauth \
+	-skip qtpurchasing \
+	-skip qtquick3d \
+	-skip qtquicktimeline \
+	-skip qtremoteobjects \
+	-skip qtscript \
+	-skip qtscxml \
+	-skip qtsensors \
+	-skip qtserialbus \
+	-skip qtserialport \
+	-skip qtspeech \
+	-skip qtvirtualkeyboard \
+	-skip qtwebchannel \
+	-skip qtwebengine \
+	-skip qtwebglplugin \
+	-skip qtwebsockets \
+	-skip qtwinextras \
+	-skip qtx11extras \
+	-skip qtxmlpatterns 
+
+#-skip qtgraphicaleffects \
